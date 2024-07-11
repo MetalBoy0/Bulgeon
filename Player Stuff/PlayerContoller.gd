11ffe42474b8dd1c2e4f3f3d45bc07717bd16044
MetalBoy0 <will.v.garrison@gmail.com>
2024-07-11 11:55:43 -0500

    Tweaked drag to replace stopping movement

--- Player Stuff/PlayerContoller.gd
+++ Player Stuff/PlayerContoller.gd
@@ -9,23 +9,29 @@ extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 
+var isMoving: bool = false
+
 func _physics_process(delta):
 	# Apply drag
-	velocity.x /= (1+drag*delta)
 	
+	if !isMoving:
+		velocity.x /= (1+drag*delta)
+		print(velocity.x)
+	
+	isMoving = false
 	# Process inputs
 	if Input.is_action_pressed("move_left"):
 		velocity.x = max(velocity.x - acceleration, -maxSpeed) 
-	elif Input.is_action_just_released("move_left"):
-		velocity.x = 0
+		isMoving = true
 		
 	if Input.is_action_pressed("move_right"):
 		velocity.x = min(velocity.x + acceleration, maxSpeed)
-	elif Input.is_action_just_released("move_right"):
-		velocity.x = 0
+		isMoving = true
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y -= jumpForce
+	
+	
 		
 	# Gravity
 	if !is_on_floor():
