bfaecfea43c0540ef373ae41deb010f5b5ff14c5
MetalBoy0 <will.v.garrison@gmail.com>
2024-07-11 11:39:08 -0500

    A simple drag implemeted
    This is because we don't have a system to slow the player down if an external force hits them

--- Player Stuff/PlayerContoller.gd
+++ Player Stuff/PlayerContoller.gd
@@ -1,25 +1,32 @@
 extends CharacterBody2D
 
-@export var maxSpeed: float = 500
-@export var acceleration: float = 50; 
-@export var gravity: float = 1000
-@export var jumpForce: float = 500
+# Adjust these in the characterbody inpector
+@export var maxSpeed: float
+@export var acceleration: float 
+@export var gravity: float
+@export var jumpForce: float
+@export var drag: float # Note: Currently drag only affects horizontal movement
 
 # Called when the node enters the scene tree for the first time.
 
 func _physics_process(delta):
+	# Apply drag
+	velocity.x /= (1+drag*delta)
+	
 	# Process inputs
 	if Input.is_action_pressed("move_left"):
 		velocity.x = max(velocity.x - acceleration, -maxSpeed) 
 	elif Input.is_action_just_released("move_left"):
 		velocity.x = 0
+		
 	if Input.is_action_pressed("move_right"):
-		velocity.x = min(velocity.x + acceleration, maxSpeed) 
+		velocity.x = min(velocity.x + acceleration, maxSpeed)
 	elif Input.is_action_just_released("move_right"):
 		velocity.x = 0
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y -= jumpForce
+		
 	# Gravity
 	if !is_on_floor():
 		velocity.y += gravity * delta
