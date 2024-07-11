59da13e9447137203b14a132be469aa6ca7c9a28
MetalBoy0 <will.v.garrison@gmail.com>
2024-07-11 11:00:00 -0500

    Added jumping and tweaked move settings

--- Player Stuff/PlayerContoller.gd
+++ Player Stuff/PlayerContoller.gd
@@ -2,10 +2,13 @@ extends CharacterBody2D
 
 @export var maxSpeed: float = 500
 @export var acceleration: float = 50; 
+@export var gravity: float = 1000
+@export var jumpForce: float = 500
 
 # Called when the node enters the scene tree for the first time.
 
 func _physics_process(delta):
+	# Process inputs
 	if Input.is_action_pressed("move_left"):
 		velocity.x = max(velocity.x - acceleration, -maxSpeed) 
 	elif Input.is_action_just_released("move_left"):
@@ -14,6 +17,12 @@ func _physics_process(delta):
 		velocity.x = min(velocity.x + acceleration, maxSpeed) 
 	elif Input.is_action_just_released("move_right"):
 		velocity.x = 0
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y -= jumpForce
+	# Gravity
+	if !is_on_floor():
+		velocity.y += gravity * delta
 	move_and_slide()
 		
 	
@@ -25,5 +34,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(position)
 	pass
